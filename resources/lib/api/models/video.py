b3ca11ad2ffefdd0a1f5ed99af8d4918f2c6e6cf
MatiasStorm <mat-435@hotmail.com>
2021-02-14 10:06:50 +0100

    Reformat video title string

--- resources/lib/api/models/video.py
+++ resources/lib/api/models/video.py
@@ -10,14 +10,17 @@ class Video(Node):
         Node.__init__(self, title=title, plot=plot, thumb=thumb, id=id)
 
         self.video = video
-        self.episode_number = video.get("episodeNumber", None)
-        self.season_number = video.get("seasonNumber", None)
+        episode_number = video.get("episodeNumber", None)
+        self.episode_number = "0" + str(episode_number) if episode_number < 10 else str(episode_number)
+        season_number = video.get("seasonNumber", None)
+        self.season_number = "0" + str(season_number) if season_number < 10 else str(season_number)
+
         self.publication_date = None
         if video.get("firstPublicationDate", None) != None:
             self.publication_date = datetime.fromtimestamp(int(video.get("firstPublicationDate", None)) / 1000).strftime("%d.%m.%Y")
         self.watched = video.get("watched", None)
         if self.episode_number != None and self.season_number != None:
-            self.title = "S%d/E%d - %s" % ( self.season_number, self.episode_number, self.title )
+            self.title = "%sx%s - %s" % ( self.season_number, self.episode_number, self.title )
 
     def get_publication_date(self):
         return self.publication_date
